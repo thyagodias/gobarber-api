d4dd05e75164600c9251cd9507cc0b0e225a417d
Thyago Dias <thyagojuniorrochadias@gmail.com>
2020-05-13 11:41:09 -0400

    altera migration para gerar uuid do agendamento

--- src/database/migrations/1589374977379-CreateAppointments.ts
+++ src/database/migrations/1589374977379-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1589374977379
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
